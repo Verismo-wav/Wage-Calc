7f80cf12b640ab3eed33cd7aeec002b193647bb5
Verismo-wav <deej17424@gmail.com>
2026-02-02 22:06:04 -0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WageCalculator from './WageCalculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WageCalculator />
+  </React.StrictMode>,
+)
